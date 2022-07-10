411007c1923fc4bb94b05524615cf930d256362a
Danilo Carrasco <danilo.carrasco.95@gmail.com>
2022-07-10 18:17:05 -0400

    ruta general GET * para rutas fuera de la API

--- index.js
+++ index.js
@@ -16,3 +16,10 @@ app.listen(PORT, () => console.log(`Server ON, http://localhost:${PORT}`));
 
 app.use("/api/v1/product", productRoute);
 app.use("/api/v1/category", categoryRoute);
+
+app.get("*", (req, res) => {
+    res.send({
+      code: '404 Recurso no encontrado',
+      message: 'La ruta que esta buscando no forma parte del API REST'
+    });
+});
